8492974751ae808022421a29d58eedebde4fc6ba
salmanhaider2390-lab <salmanhaider2390@gmail.com>
2026-07-11 17:39:08 +0100

    tests defnition

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -1,1 +1,24 @@
 #tests
+
+import os
+import sys
+import tempfile
+import pytest
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from app import create_app,API_KEY
+from models import db
+
+@pytest.fixture
+def client():
+    fd, path = tempfile.mkstemp()
+    app = create_app(db_path=path)
+    app.config["TESTING"] = True
+
+    with app.test_client() as client:
+        yield client
+
+    os.close(fd)
+    os.unlink()
+HEADERS = {"X-API-KEY": API_KEY, "Content_Type": "application/json"}
+
